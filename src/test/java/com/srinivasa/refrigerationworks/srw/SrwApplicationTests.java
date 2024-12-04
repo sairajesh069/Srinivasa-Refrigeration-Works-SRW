8f7489fd3db9a98a5cb3edb4866b10b45f2eb156
sai rajesh <sairajesh.srk@gmail.com>
2024-12-04 08:15:59 +0530

    Initialized a Spring Boot project using start.spring.io and extracted it into IntelliJ.

---
+++ src/test/java/com/srinivasa/refrigerationworks/srw/SrwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinivasa.refrigerationworks.srw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
