c94fa0f0eec561b6a8c681b117263f9e80da7049
sai rajesh <sairajesh.srk@gmail.com>
2024-12-05 21:19:25 +0530

    Added '/SRW/employee/register' and '/SRW/employee/confirmation' endpoints in UserCredentialController for registration and confirmation pages; added required utility methods and model.

--- src/main/java/com/srinivasa/refrigerationworks/srw/controller/UserCredentialController.java
+++ src/main/java/com/srinivasa/refrigerationworks/srw/controller/UserCredentialController.java
@@ -1,6 +1,7 @@
 package com.srinivasa.refrigerationworks.srw.controller;
 
 import com.srinivasa.refrigerationworks.srw.model.UserCredentialModel;
+import com.srinivasa.refrigerationworks.srw.payload.dto.EmployeeCredentialDTO;
 import com.srinivasa.refrigerationworks.srw.payload.dto.OwnerCredentialDTO;
 import com.srinivasa.refrigerationworks.srw.service.UserCredentialService;
 import com.srinivasa.refrigerationworks.srw.utility.common.StringEditor;
@@ -45,10 +46,33 @@ public class UserCredentialController {
     @PostMapping("/owner/confirmation")
     public String confirmOwner(@ModelAttribute @Valid OwnerCredentialDTO ownerCredentialDTO, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            UserCredentialModel.addOwnerFormConstantsToModel(model);
+            UserCredentialModel.addUserFormConstantsToModel(model);
             return "owner/owner-register-form";
         }
         userCredentialService.addOwnerCredential(ownerCredentialDTO);
         return "owner/owner-confirmation";
     }
+
+    /*
+     * Displays the employee registration form
+     */
+    @GetMapping("/employee/register")
+    public String createEmployee(Model model) {
+        UserCredentialModel.addEmployeeCredentialToModel(model);
+        return "employee/employee-register-form";
+    }
+
+    /*
+     * Confirms the employee registration and adds employee credentials
+     */
+    @PostMapping("/employee/confirmation")
+    public String confirmEmployee(@ModelAttribute @Valid EmployeeCredentialDTO employeeCredentialDTO, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            UserCredentialModel.addUserFormConstantsToModel(model);
+            return "employee/employee-register-form";
+        }
+        userCredentialService.addEmployeeCredential(employeeCredentialDTO);
+        return "employee/employee-confirmation";
+    }
+
 }
--- src/main/java/com/srinivasa/refrigerationworks/srw/model/UserCredentialModel.java
+++ src/main/java/com/srinivasa/refrigerationworks/srw/model/UserCredentialModel.java
@@ -1,8 +1,6 @@
 package com.srinivasa.refrigerationworks.srw.model;
 
-import com.srinivasa.refrigerationworks.srw.payload.dto.OwnerCredentialDTO;
-import com.srinivasa.refrigerationworks.srw.payload.dto.OwnerDTO;
-import com.srinivasa.refrigerationworks.srw.payload.dto.UserCredentialDTO;
+import com.srinivasa.refrigerationworks.srw.payload.dto.*;
 import com.srinivasa.refrigerationworks.srw.utility.common.constants.UserFormConstants;
 import org.springframework.ui.Model;
 
@@ -20,13 +18,25 @@ public class UserCredentialModel {
         ownerCredentialDTO.setOwnerDTO(new OwnerDTO());
         ownerCredentialDTO.setUserCredentialDTO(new UserCredentialDTO());
         model.addAttribute("ownerCredentialDTO", ownerCredentialDTO);
-        addOwnerFormConstantsToModel(model);
+        addUserFormConstantsToModel(model);
     }
 
     /*
-     * Adds form constants (like genders) to the model for owner registration.
+     * Initializes the EmployeeDTO and UserCredentialDTO.
+     * Adds EmployeeCredentialDTO and form constants to the model for employee registration.
      */
-    public static void addOwnerFormConstantsToModel(Model model) {
+    public static void addEmployeeCredentialToModel(Model model) {
+        EmployeeCredentialDTO employeeCredentialDTO = new EmployeeCredentialDTO();
+        employeeCredentialDTO.setEmployeeDTO(new EmployeeDTO());
+        employeeCredentialDTO.setUserCredentialDTO(new UserCredentialDTO());
+        model.addAttribute("employeeCredentialDTO", employeeCredentialDTO);
+        addUserFormConstantsToModel(model);
+    }
+
+    /*
+     * Adds form constants (like genders) to the model for user registration.
+     */
+    public static void addUserFormConstantsToModel(Model model) {
         model.addAttribute("genders", UserFormConstants.GENDERS);
     }
 }
