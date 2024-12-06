dbb77c6b7e2f7ddff7b0020f44275a1f967531f0
sai rajesh <sairajesh.srk@gmail.com>
2024-12-06 22:44:07 +0530

    Added query in UserCredentialRepository to fetch username by phone number.

--- src/main/java/com/srinivasa/refrigerationworks/srw/repository/UserCredentialRepository.java
+++ src/main/java/com/srinivasa/refrigerationworks/srw/repository/UserCredentialRepository.java
@@ -2,6 +2,8 @@ package com.srinivasa.refrigerationworks.srw.repository;
 
 import com.srinivasa.refrigerationworks.srw.entity.UserCredential;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /*
@@ -10,4 +12,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserCredentialRepository extends JpaRepository<UserCredential, String> {
+
+    /*
+     * Query to fetch the username associated with the given phone number.
+     * - Returns the username as a string.
+     */
+    @Query("SELECT username FROM UserCredential WHERE phoneNumber = :phoneNumber")
+    public String fetchUsernameByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 }
