d8766eb84723afb06929c9407de09e1a652144af
sai rajesh <sairajesh.srk@gmail.com>
2024-12-20 10:24:16 +0530

    Added UserStatus utility enum class.

---
+++ src/main/java/com/srinivasa/refrigerationworks/srw/utility/common/enums/UserStatus.java
@@ -0,0 +1,10 @@
+package com.srinivasa.refrigerationworks.srw.utility.common.enums;
+
+/*
+ * Enum representing the status of a user.
+ * - ACTIVE: Indicates the user is active.
+ * - IN_ACTIVE: Indicates the user is inactive.
+ */
+public enum UserStatus {
+    ACTIVE, IN_ACTIVE;
+}
