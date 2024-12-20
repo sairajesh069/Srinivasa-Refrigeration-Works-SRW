4f1c65d052916daf6ab331299ec3c538113d64c7
sai rajesh <sairajesh.srk@gmail.com>
2024-12-20 17:33:33 +0530

    Added method in OwnerRepository to deactivate owner.

--- src/main/java/com/srinivasa/refrigerationworks/srw/repository/OwnerRepository.java
+++ src/main/java/com/srinivasa/refrigerationworks/srw/repository/OwnerRepository.java
@@ -1,10 +1,15 @@
 package com.srinivasa.refrigerationworks.srw.repository;
 
 import com.srinivasa.refrigerationworks.srw.entity.Owner;
+import com.srinivasa.refrigerationworks.srw.utility.common.enums.UserStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 /*
  * Repository interface for Owner entity
@@ -19,4 +24,13 @@ public interface OwnerRepository extends JpaRepository<Owner, Long> {
      */
     @Query("SELECT o FROM Owner o WHERE o.ownerId = :identifier OR o.phoneNumber = :identifier OR o.email = :identifier")
     public Owner findByIdentifier(@Param("identifier") String identifier);
+
+    /*
+     * Deactivates an owner by updating their status and 'updatedAt' field.
+     * - Updates the Owner entity for the specified ownerId.
+     */
+    @Modifying
+    @Transactional
+    @Query("UPDATE Owner SET updatedAt = :updatedAt, status = :status WHERE ownerId = :ownerId")
+    public void deactivateOwner(@Param("ownerId") String ownerId, @Param("updatedAt") LocalDateTime updatedAt, @Param("status") UserStatus status);
 }
