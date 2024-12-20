ded3fa7a95fe0d8fabd7884b8a58151818e531e9
sai rajesh <sairajesh.srk@gmail.com>
2024-12-20 19:20:14 +0530

    Added method in ComplaintRepository to deactivate complaint.

--- src/main/java/com/srinivasa/refrigerationworks/srw/repository/ComplaintRepository.java
+++ src/main/java/com/srinivasa/refrigerationworks/srw/repository/ComplaintRepository.java
@@ -1,9 +1,15 @@
 package com.srinivasa.refrigerationworks.srw.repository;
 
 import com.srinivasa.refrigerationworks.srw.entity.Complaint;
+import com.srinivasa.refrigerationworks.srw.utility.common.enums.ComplaintState;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /*
@@ -23,4 +29,13 @@ public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
      * Finds and returns a Complaint entity by its complaintId.
      */
     public Complaint findByComplaintId(String complaintId);
+
+    /*
+     * Deactivates a complaint by updating their state and 'updatedAt' fields.
+     * - Updates the Complaint entity for the specified complaintId.
+     */
+    @Modifying
+    @Transactional
+    @Query("UPDATE Complaint SET updatedAt = :updatedAt, state = :state WHERE complaintId = :complaintId")
+    public void deactivateComplaint(@Param("complaintId") String complaintId, @Param("updatedAt") LocalDateTime updatedAt, @Param("state") ComplaintState state);
 }
