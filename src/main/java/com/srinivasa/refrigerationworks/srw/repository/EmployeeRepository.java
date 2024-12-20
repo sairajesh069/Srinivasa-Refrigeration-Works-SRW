468ea3f193576b3baab8a5d9dea14f6b803e0e1c
sai rajesh <sairajesh.srk@gmail.com>
2024-12-20 17:57:00 +0530

    Added method in EmployeeRepository to deactivate employee.

--- src/main/java/com/srinivasa/refrigerationworks/srw/repository/EmployeeRepository.java
+++ src/main/java/com/srinivasa/refrigerationworks/srw/repository/EmployeeRepository.java
@@ -1,10 +1,15 @@
 package com.srinivasa.refrigerationworks.srw.repository;
 
 import com.srinivasa.refrigerationworks.srw.entity.Employee;
+import com.srinivasa.refrigerationworks.srw.utility.common.enums.UserStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 /*
  * Repository interface for Employee entity
@@ -19,4 +24,13 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
      */
     @Query("SELECT e FROM Employee e WHERE e.employeeId = :identifier OR e.phoneNumber = :identifier OR e.email = :identifier or e.nationalIdNumber = :identifier")
     public Employee findByIdentifier(@Param("identifier") String identifier);
+
+    /*
+     * Deactivates an employee by updating their status, 'updatedAt' and 'dateOfExit' fields.
+     * - Updates the Employee entity for the specified employeeId.
+     */
+    @Modifying
+    @Transactional
+    @Query("UPDATE Employee SET updatedAt = :timeStamp, dateOfExit = :timeStamp, status = :status WHERE employeeId = :employeeId")
+    public void deactivateEmployee(@Param("employeeId") String employeeId, @Param("timeStamp") LocalDateTime timeStamp, @Param("status") UserStatus status);
 }
