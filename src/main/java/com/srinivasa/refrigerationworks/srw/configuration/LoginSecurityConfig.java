45eddf77ef290580845cd1a7c232e52737ee4efd
sai rajesh <sairajesh.srk@gmail.com>
2024-12-24 14:32:31 +0530

    Removed access restriction for update page and replaced 'disabled' with 'readonly' in employee-update page.

--- src/main/java/com/srinivasa/refrigerationworks/srw/configuration/LoginSecurityConfig.java
+++ src/main/java/com/srinivasa/refrigerationworks/srw/configuration/LoginSecurityConfig.java
@@ -103,7 +103,6 @@ public class LoginSecurityConfig {
                          * - "/SRW/employee/list", "/SRW/customer/list", "/SRW/complaint/list": Access to various list pages.
                          * - "/SRW/employee/active-list", "/SRW/customer/active-list", "/SRW/complaint/active-list": Access to various active-list pages.
                          * - "/SRW/employee/search", "/SRW/customer/search": Access to various search pages.
-                         * - "/SRW/employee/update", "/SRW/customer/update": Access to various update pages.
                          * - "/SRW/employee/activate", "/SRW/customer/activate", "/SRW/complaint/activate": Access to various activate pages.
                          * - "/SRW/employee/deactivate", "/SRW/customer/deactivate", "/SRW/complaint/deactivate": Access to various deactivate pages.
                          */
@@ -112,7 +111,6 @@ public class LoginSecurityConfig {
                         .requestMatchers("/SRW/employee/list", "/SRW/customer/list", "/SRW/complaint/list").hasRole("OWNER")
                         .requestMatchers("/SRW/employee/active-list", "/SRW/customer/active-list", "/SRW/complaint/active-list").hasRole("OWNER")
                         .requestMatchers("/SRW/employee/search", "/SRW/customer/search").hasRole("OWNER")
-                        .requestMatchers("/SRW/employee/update", "/SRW/customer/update").hasRole("OWNER")
                         .requestMatchers("/SRW/employee/activate", "/SRW/customer/activate", "/SRW/complaint/activate").hasRole("OWNER")
                         .requestMatchers("/SRW/employee/deactivate", "/SRW/customer/deactivate", "/SRW/complaint/deactivate").hasRole("OWNER")
 
