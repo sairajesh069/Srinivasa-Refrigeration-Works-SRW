e3bd5fed79c82881a2510474c1d46525478b3043
sai rajesh <sairajesh.srk@gmail.com>
2025-01-05 11:18:00 +0530

    Created EmployeeInfoDTO with required fields.

---
+++ src/main/java/com/srinivasa/refrigerationworks/srw/payload/dto/EmployeeInfoDTO.java
@@ -0,0 +1,43 @@
+package com.srinivasa.refrigerationworks.srw.payload.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/*
+ * DTO class for transferring Employee info.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeInfoDTO implements Serializable {
+
+    /*
+     * Unique ID for serialization compatibility.
+     */
+    @Serial
+    private static final long serialVersionUID = 42L;
+
+    /*
+     * Employee's unique Id.
+     */
+    private String employeeId;
+
+    /*
+     * Employee's full name.
+     */
+    private String fullName;
+
+    /*
+     * Employee's phone number.
+     */
+    private String phoneNumber;
+
+    /*
+     * Employee's designation.
+     */
+    private String designation;
+}
